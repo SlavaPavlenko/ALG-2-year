1d6eb5e6794481fdaccde439ebf8adf4565b63dd
SunRay <slava.pavlenko2010@yandex.ru>
2018-12-19 17:01:31 +0400

    03 lab

---
+++ 03/03/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mas = new int[10] {3, 12, 2, 7, 2, 1, 7, 3, 5, 1};
+            int res = 1;
+            for (int i = 0; i < mas.Length; i++)
+                res *=mas[i];
+            Console.WriteLine("Результат: " + res);
+            Console.Read();
+        }
+    }
+}
